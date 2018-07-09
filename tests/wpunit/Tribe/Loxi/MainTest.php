833a19d7def244c05810c7f220db1d007b176007
Scott Kingsley Clark <lol@scottkclark.com>
2018-07-09 18:39:37 -0500

    Add snapshot tests for oembed

--- tests/wpunit/Tribe/Loxi/MainTest.php
+++ tests/wpunit/Tribe/Loxi/MainTest.php
@@ -5,11 +5,23 @@ namespace Tribe\Loxi;
 use Codeception\TestCase\WPTestCase;
 use Tribe__Loxi__Main;
 use Tribe__Loxi__Shortcode;
+use Spatie\Snapshots\MatchesSnapshots;
+use tad\WPBrowser\Snapshot\WPHtmlOutputDriver;
 
 /**
  * Class MainTest
  */
 class MainTest extends WPTestCase {
+    use MatchesSnapshots;
+
+    public function setUp() {
+        // before
+        parent::setUp();
+
+        // snapshots
+        $this->driver = new WPHtmlOutputDriver( home_url(), 'http://tribe.dev' );
+    }
+
 	/**
 	 * It should hook into the init action
 	 * @test
@@ -46,6 +58,106 @@ class MainTest extends WPTestCase {
 		$provider_existed = wp_oembed_remove_provider( 'https://*.loxi.io/*' );
 
 		$this->assertTrue( $provider_existed );
+	}
+
+	/**
+	 * It should embed the url for single event
+	 *
+	 * @test
+	 */
+	public function it_should_embed_the_url_for_single_event() {
+		$sut = $this->make_instance();
+
+		$sut->init();
+
+		$content = '
+Some cool stuff below.
+
+Here is the calendar, check it out:
+
+https://wpshindig.loxi.io/wordpress-denver-happiness-hour-8-1264
+		';
+
+		$output = apply_filters( 'the_content', $content, 0 );
+
+        $this->assertMatchesSnapshot( $output, $this->driver );
+
+        $this->assertContains( 'subdomain="wpshindig"', $output );
+	}
+
+	/**
+	 * It should embed the url for list
+	 *
+	 * @test
+	 */
+	public function it_should_embed_the_url_for_list() {
+		$sut = $this->make_instance();
+
+		$sut->init();
+
+		$content = '
+Some cool stuff below.
+
+Here is the calendar, check it out:
+
+https://wpshindig.loxi.io/list/2018/7
+		';
+
+		$output = apply_filters( 'the_content', $content, 0 );
+
+        $this->assertMatchesSnapshot( $output, $this->driver );
+
+        $this->assertContains( 'subdomain="wpshindig"', $output );
+	}
+
+	/**
+	 * It should embed the url for home
+	 *
+	 * @test
+	 */
+	public function it_should_embed_the_url_for_home() {
+		$sut = $this->make_instance();
+
+		$sut->init();
+
+		$content = '
+Some cool stuff below.
+
+Here is the calendar, check it out:
+
+https://wpshindig.loxi.io
+		';
+
+		$output = apply_filters( 'the_content', $content, 0 );
+
+        $this->assertMatchesSnapshot( $output, $this->driver );
+
+        $this->assertContains( 'subdomain="wpshindig"', $output );
+	}
+
+	/**
+	 * It should embed the url for home with slash
+	 *
+	 * @test
+	 */
+	public function it_should_embed_the_url_for_home_with_slash() {
+		$sut = $this->make_instance();
+
+		$sut->init();
+
+		$content = '
+Some cool stuff below.
+
+Here is the calendar, check it out:
+
+https://wpshindig.loxi.io/
+		';
+
+		$output = apply_filters( 'the_content', $content, 0 );
+
+        $this->assertMatchesSnapshot( $output, $this->driver );
+
+        $this->assertContains( 'subdomain="wpshindig"', $output );
 	}
 
 	/**
